61a4cd56fe7ceab21dab299c16185a5576138c08
jacksonCLyu <yucl930904@163.com>
2018-08-21 22:30:18 +0800

    添加hbase表常量类
    Signed-off-by: jacksonCLyu <yucl930904@163.com>

--- src/main/java/com/spellchain/passbook/constant/Constants.java
+++ src/main/java/com/spellchain/passbook/constant/Constants.java
@@ -43,4 +43,94 @@ public class Constants {
         /** 住址 */
         public static final String ADDRESS = "address";
     }
+
+    /**
+     * <h2>PassTemplate HBase Table</h2>
+     * */
+    public class PassTemplateTable{
+
+        /** PassTemplate HBase 表名 */
+        public static final String TABLE_NAME = "pb:passtemplate";
+
+        /** 基本信息列族 */
+        public static final String FAMILY_B = "b";
+
+        /** 商户 id */
+        public static final String ID = "id";
+
+        /** 优惠券标题 */
+        public static final String TITLE = "title";
+
+        /** 优惠券标题 */
+        public static final String DESC = "desc";
+
+        /** 优惠券是否有 token */
+        public static final String HAS_TOKEN = "has_token";
+
+        /** 优惠券背景色 */
+        public static final String BACKGROUND = "background";
+
+        /** 约束信息列族 */
+        public static final String FAMILY_C = "c";
+
+        /** 最大个数限制 */
+        public static final String LIMIT = "limit";
+
+        /** 优惠券开始时间 */
+        public static final String START = "start";
+
+        /** 优惠券结束时间 */
+        public static final String END = "end";
+    }
+
+    /**
+     * <h2>Pass HBase Table</h2>
+     * */
+    public class PassTable{
+
+        /** Pass HBase 表名 */
+        public static final String TABLE_NAME = "pb:pass";
+
+        /** 信息列族 */
+        public static final String FAMILY_I = "i";
+
+        /** 用户 id */
+        public static final String USER_ID = "user_id";
+
+        /** 优惠券 id */
+        public static final String TEMPLATE_ID = "template_id";
+
+        /** 优惠券识别码 */
+        public static final String TOKEN = "token";
+
+        /** 优惠券领取日期 */
+        public static final String ASSIGNED_DATE = "assigned_date";
+
+        /** 消费日期 */
+        public static final String CON_DATE = "con_date";
+    }
+
+    /**
+     * <h2>Feedback HBase Table</h2>
+     */
+    public class FeedbackTable{
+
+        /** Feedback HBase 表名 */
+        public static final String TABLE_NAME = "pb:feedback";
+
+        /** 信息列族 */
+        public static final String FAMILY_I = "i";
+
+        /** 用户 id */
+        public static final String USER_ID = "user_id";
+
+        /** 评论类型 */
+        public static final String TYPE = "type";
+
+        /** PassTemplate RowKey,如果是 app 评论,则是 -1 */
+        public static final String TEMPLATE_ID = "template_id";
+
+        /** 评论内容 */
+        public static final String COMMENT = "comment";
+    }
 }
