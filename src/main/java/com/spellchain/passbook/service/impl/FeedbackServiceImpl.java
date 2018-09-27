b16d21711d087d66ef9893bfd47c6deafe8f8eef
young <yucl930904@163.com>
2018-09-27 23:23:51 +0800

    unused import
    Signed-off-by: young <yucl930904@163.com>

--- src/main/java/com/spellchain/passbook/service/impl/FeedbackServiceImpl.java
+++ src/main/java/com/spellchain/passbook/service/impl/FeedbackServiceImpl.java
@@ -13,7 +13,6 @@ import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.filter.PrefixFilter;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
